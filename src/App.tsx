b49ce2db4f10d4e7197650dcf5da64cdd2490f40
Leinard Grande <leinard.grande@ucalgary.ca>
2024-01-24 22:57:41 -0700

    revamp finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import '@radix-ui/themes/styles.css';
 import "./App.css";
-import OldPicker from './radix-ui-datetime-picker/old/DateTimePicker';
 import { Button, Flex, Popover, TextField } from '@radix-ui/themes';
 import { useState } from 'react';
 import { CalendarIcon } from '@radix-ui/react-icons';
@@ -12,46 +11,6 @@ const [date, setDate] = useState<Date>(new Date());
 
 return (
 	<div className="h-screen w-screen flex justify-center items-center gap-2">
-		<div>
-			<Flex justify="center" align="center" mb="6">
-
-				<Popover.Root>
-
-				<Popover.Trigger>
-					<TextField.Root>
-						<TextField.Input value={date.toLocaleString("en-GB", {
-							day: "numeric",
-							month: "short",
-							year: "numeric",
-							hour: "numeric",
-							minute: "numeric",
-							second: undefined
-						})} readOnly />
-						<TextField.Slot pr="0">
-							<Button variant="soft">
-							<CalendarIcon />
-							</Button>
-						</TextField.Slot>
-					</TextField.Root>
-				</Popover.Trigger>
-
-				<Popover.Content>
-					<OldPicker timeLabel="Available Times" label="Select date and time" onChange={setDate} dateTimePickerOptions={{
-						timeOptions: {
-						from: 9 * 60,
-						to: 21 * 60,
-						interval: 30
-						},
-						sunday: {
-						from: 10 * 60,
-						to: 17 * 60,
-						}
-					}} />
-				</Popover.Content>
-				
-				</Popover.Root>
-			</Flex>
-		</div>
 
 		<div>
 			<Flex justify="center" align="center" mb="6">
